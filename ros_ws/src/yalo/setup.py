c38621c426d442e24215828182817c09adb34b92
achyutsun <achyutsun@users.noreply.github.com>
2026-03-06 17:43:06 -0700

    ROS 2 source code
    ROS 2 source code

---
+++ ros_ws/src/yalo/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'yalo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='YOLO Team',
+    maintainer_email='achyutros@gmail.com',
+    description='YALO Mobile Robot',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'yalo = yalo.yalo:main'
+        ],
+    },
+)
