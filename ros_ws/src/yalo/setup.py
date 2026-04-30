37c87494f1f06b9d1bad3751bd43fac9ce57832c
achyutsun <achyutsun@users.noreply.github.com>
2026-04-29 20:00:37 -0700

    Update setup.py

--- ros_ws/src/yalo/setup.py
+++ ros_ws/src/yalo/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 from glob import glob
+import os
 
 package_name = 'yalo'
 
@@ -11,8 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.py')),
-        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        ('share/' + package_name + '/rviz', ['rviz/exploration.rviz']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -30,6 +32,7 @@ setup(
             'yalo = yalo.yalo:main',
             'decision_maker = yalo.decision_maker:main',
             'frontier_detector = yalo.frontier_detector:main',
+            'entropy_explorer = yalo.entropy_explorer:main',
         ],
     },
 )
